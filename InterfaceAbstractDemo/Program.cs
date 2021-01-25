e61d78beba1e152e07b9bc615f044f6b63f2d791
Furkan ARTAR <asosyalgenctvtr@gmail.com>
2021-01-25 19:06:52 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using InterfaceAbstractDemo.Adapters;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager baseCustomerManager = new NeroCustomerManager();
+            baseCustomerManager.Save(new Customer { DateOfBirth = new DateTime(1999, 1, 1), FirstName = "Furkan", LastName = "ARTAR", NationalityId = 12345678910 });
+            Console.ReadLine();
+        }
+    }
+}
